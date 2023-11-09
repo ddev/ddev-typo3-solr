e9b54275d13d98f77720db59b2f36218d70dd79b
Jochen <rothjochen@gmail.com>
2023-11-09 17:32:23 +0100

    [TASK] Activate all tests

--- tests/setup_suite.bash
+++ tests/setup_suite.bash
@@ -1,26 +1,27 @@
 #!/usr/bin/env bats
 
 setup_suite() {
-    export DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
-    set -eu -o pipefail
-    export TESTDIR=~/tmp/test-apache-solr-addon
-    mkdir -p $TESTDIR
-    export PROJNAME=addon-apache-solr
-    export DDEV_NON_INTERACTIVE=true
-    ddev delete -Oy ${PROJNAME} >/dev/null 2>&1 || true
-    cd "${TESTDIR}"
-    ddev config --project-name=${PROJNAME}
-    ddev start -y >/dev/null
+  export DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
+  set -eu -o pipefail
+  export TESTDIR=~/tmp/test-apache-solr-addon
+  mkdir -p $TESTDIR
+  export PROJNAME=test-addon-apache-solr
+  export DDEV_NON_INTERACTIVE=true
 
-    cd "${TESTDIR}"
-    echo "# ddev get ${DIR} with project ${PROJNAME} in ${TESTDIR} ($(pwd))" >&3
-    ddev get "${DIR}/.."
-    ddev restart
+  ddev delete -Oy ${PROJNAME} >/dev/null 2>&1 || true
+  cd "${TESTDIR}"
+  ddev config --project-name=${PROJNAME}
+  ddev start -y >/dev/null
+
+  echo "# ddev get ${DIR} with project ${PROJNAME} in ${TESTDIR} ($(pwd))" >&3
+  ddev get "${DIR}/.."
+  cp -rf "$DIR" "$TESTDIR/"
+  ddev restart -y >/dev/null
 }
 
 teardown_suite() {
   set -eu -o pipefail
   cd ${TESTDIR} || ( printf "unable to cd to ${TESTDIR}\n" && exit 1 )
   ddev delete -Oy ${PROJNAME} >/dev/null 2>&1
-  [ "${TESTDIR}" != "" ] && rm -rf ${TESTDIR}
+  [ "${TESTDIR}" != "" ] && sudo rm -rf ${TESTDIR}
 }
